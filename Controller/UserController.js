abdad1882204b9c01d3dbd2330f1a3bebb460d7d
DESKTOP-TP9KLF6\TranNam <trantunam.cqtk8@gmail.com>
2017-02-19 23:17:40 +0700

    test

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -11,13 +11,13 @@ module.exports = function(app, firebase) {
     });
 
     app.post(url, function(req, res) {  
-        if (req.headers['authen'] == null) 
-            return res.json({
-							responseCode : -1,
-							description : "",
-							data : ""
-						});
-        userDAO.update(firebase, req.headers['authen'], req.body.email, req.body.firstName, req.body.lastName, 
+        // if (req.headers['authen'] == null) 
+        //     return res.json({
+		// 					responseCode : -1,
+		// 					description : "",
+		// 					data : ""
+		// 				});
+        userDAO.update(firebase, req.body.authen, req.body.email, req.body.firstName, req.body.lastName, 
             req.body.gender, req.body.avatar, req.body.dateOfBirth, function(result) {
             res.json(result); 
         });
