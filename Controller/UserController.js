37c23cf66feb1456b348ed500eaba7e8512d30de
DESKTOP-TP9KLF6\TranNam <trantunam.cqtk8@gmail.com>
2017-02-19 23:46:38 +0700

    .

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -10,14 +10,14 @@ module.exports = function(app, firebase) {
         });
     });
 
-    app.post(url, function(req, res) {  
-        // if (req.headers['authen'] == null) 
-        //     return res.json({
-		// 					responseCode : -1,
-		// 					description : "",
-		// 					data : ""
-		// 				});
-        userDAO.update(firebase, req.body.authen, req.body.email, req.body.firstName, req.body.lastName, 
+    app.put(url, function(req, res) {  
+        if (req.headers['authen'] == null) 
+            return res.json({
+							responseCode : -1,
+							description : "",
+							data : ""
+						});
+        userDAO.update(firebase, req.headers['authen'], req.body.email, req.body.firstName, req.body.lastName, 
             req.body.gender, req.body.avatar, req.body.dateOfBirth, function(result) {
             res.json(result); 
         });
