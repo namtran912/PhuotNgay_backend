cbf45a475b074e53904ff609743ca8c426333a74
DESKTOP-TP9KLF6\TranNam <trantunam.cqtk8@gmail.com>
2017-02-19 16:11:38 +0700

    update api login

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -4,7 +4,7 @@ module.exports = function(app, firebase) {
     var userDAO = new UserDAO();
     var url = '/api/user';   
 
-    app.post(url + '/login', function(req, res) {  
+    app.post(url + '/register', function(req, res) {  
         userDAO.login(firebase, req.body.firebaseUid, req.body.fbId, req.body.email, function(result) {
             res.json(result); 
         });
