3b16d90f5371c1c4de169dbb837a892a18644e4e
DESKTOP-TP9KLF6\TranNam <trantunam.cqtk8@gmail.com>
2017-05-14 15:53:22 +0700

    add map check security

--- Model/MapDAO.js
+++ Model/MapDAO.js
@@ -8,9 +8,10 @@ module.exports = function() {
 	this.MapDAO = function() {
 		this.ref = 'MAP/';
 		this.tripRef = 'TRIP/';
+		this.trackRef = 'TRACK/';
     }
 
-    MapDAO.prototype.checkSecurity = function(firebase, token, tripId, radius, callback) {
+    MapDAO.prototype.checkSecurity = function(firebase, token, tripId, callback) {
         var that = this;
 		helper.verifyToken(token, function(decoded){
 			if (decoded == null) 
@@ -38,27 +39,109 @@ module.exports = function() {
 							description : "Authen is expired"
 						});
 
-				firebase.database().ref(that.ref + tripId).once('value').then(function(snapshot) {
+				firebase.database().ref(that.trackRef + tripId).once('value').then(function(snapshot) {
 					if (snapshot.val() == null) 
 						return callback({
 							responseCode : -1,
 							description : "Trip's track is not exist!"
 						});
 
-					var vectors = [];
-					var fbIds = [];
 					var track = snapshot.val();
 
-					for (fbId in track) {
-						var geo = Object.keys(track[fbId].geo);
-						var pos = track[fbId].geo[geo[geo.length - 1]].split(", ");
-						vectors.push([pos[0], pos[1]]);
-						fbIds.push(fbId);
-					}
+					firebase.database().ref(that.ref + tripId).once('value').then(function(_snapshot) {
+						if (_snapshot.val() == null) 
+							return callback({
+								responseCode : -1,
+								description : "Trip's map is not exist!"
+							});
 
-					helper.checkSecurity(radius, vectors, function(cluster){
-						for(i in cluster)
-							console.log(fbIds[cluster[i]]);
+						var security = _snapshot.val().security;
+						var cores = [];
+
+						for (i in security.core) {
+							if (!track.hasOwnProperty(security.core[i]))
+								return callback({
+									responseCode : -1,
+									description : security.core[i] + "'s track is not exist!"
+								});
+							
+							var pos = track[security.core[i]].lastGPS.split(', ');
+							cores.push(pos[0], pos[1], security.core[i]);
+						}
+
+						var result = [];
+
+						for (fbId in track) 
+							if (!security.core.includes(fbId)) {
+								var pos = track[fbId].lastGPS.split(', ');
+
+								var dMin = 0;
+								var coreMin = null;
+								for (i in cores) {
+									var dis = helper.getDistance(parseFloat(pos[0]), parseFloat(pos[1]), 
+																parseFloat(cores[i][0]), parseFloat(cores[i][1]));
+									if (dis <= radius) 
+										break;					
+									else if (dis < dMin || coreMin == null) {
+										dMin = dis;
+										coreMin = cores[i][2];
+									}
+
+								}
+
+								if (coreMin != null)	
+									result.push(fbId, coreMin);
+							}
+
+						if (result.length > 0)	
+						firebase.database().ref(that.tripRef + tripId).once('value').then(function(__snapshot) {
+							if (__snapshot.val() == null) 
+								return callback({
+									responseCode : -1,
+									description : "Trip is not exist!"
+								});
+
+							var trip = __snapshot.val();
+
+							for (i in result) {
+								userDAO.getSignInAndInfo(firebase, result[i][0], function(signIn, name, avatar) {
+								userDAO.getSignInAndInfo(firebase, result[i][1], function(signIn, _name, _avatar) {
+									var message = [];
+									message.push("Bạn đang đi quá xa với nhóm người hướng dẫn. Hãy liên hệ với người gần bạn nhất là <b>" + _name  + "</b>");
+									message.push("<b>" + name  + "</b> đang đi quá xa với nhóm người hướng dẫn. Bạn là người hướng dẫn gần nhất có thể giúp đỡ!");
+									var names = [name, _name];
+									var avatars = [avatar, _avatar];
+
+									for (j = 0; j < 2; j++) {
+										var info = {
+											from : {
+												fbId : result[i][j],
+												name : names[j], 
+												avatar : avatars[j]
+											},
+											trip : {
+												tripId : tripId,
+												cover : trip.cover,
+												name : trip.name
+											},
+											message : message[j]
+										};
+
+										userDAO.getFCM(firebase, result[i][j], function(fcm) {
+											notificationDAO.addNoti(firebase, result[i][j], info, 4, function(notiId){
+												if (notiId.success == 1) 									
+													helper.sendNoti(fcm, {}, {
+																	title : "IZIGO",
+																	body : message,
+																	icon : "#"
+																});										
+											});			
+										});
+									}
+								});
+								});
+							}
+						});
 					});
 				}); 
 			});
@@ -119,7 +202,7 @@ module.exports = function() {
 						if (!trip.members.hasOwnProperty(cores[i]) && trip.from.fbId != cores[i]) 
 							return callback({
 								responseCode : -1,
-								description : cores[i] + " is not Trip's member!"
+								description : cores[i] + " is not Trip's member or Trip's admin!"
 							});
 
 					firebase.database().ref(that.ref + tripId + '/security').set({ 
